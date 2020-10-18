e36c8105fbec69caad82ea92dc41d65d08aa15a4
creativial <creativival@gmail.com>
2020-10-18 22:31:55 +0900

    Devise のフラッシュメッセージを Bootstrap に対応させる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
+  resources :users, :only => [:index, :show]
   root 'static_pages#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
