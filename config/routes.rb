b9268df5332abb7566a248ba9ba82359ed229b69
creativival <creativival@gmail.com>
2020-10-19 23:31:12 +0900

    ログイン、ログアウト、アカウント変更後のパスを変更した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
+
+  # devise_scope :user do
+  #   get "sign_in", :to => "users/sessions#new"
+  #   get "sign_out", :to => "users/sessions#destroy"
+  # end
+
   resources :users, :only => [:index, :show]
   root 'static_pages#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
