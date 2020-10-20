2ddc0744a3bfe955f2627e2b5e457221976f8ed0
creativial <creativival@gmail.com>
2020-10-20 21:10:08 +0900

    Mini-Magic リサイズした画像を作成し、表示する
    css object-fit は不要になる

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
   validates :profile, length: { maximum: Settings.user.profile_length }
 
   has_one_attached :avatar
+
+  def display_avatar_large
+    avatar.variant(gravity: :center, resize:"240x240^", crop:"240x240+0+0").processed
+  end
+
+  def display_avatar_middle
+    avatar.variant(gravity: :center, resize:"120x120^", crop:"120x120+0+0").processed
+  end
+
+  def display_avatar_small
+    avatar.variant(gravity: :center, resize:"80x80^", crop:"80x80+0+0").processed
+  end
 end
