6c43e5c20490014a7654e6ae1ea824bf9b11dc1f
creativial <creativival@gmail.com>
2020-10-21 11:21:00 +0900

    Vanilla JS で書き換える

--- app/javascript/packs/users/edit.js
+++ app/javascript/packs/users/edit.js
@@ -1,13 +1,17 @@
-$(document).on('change', ':file', function() {
-    const size_in_megabytes = this.files[0].size/1024/1024;
+const inputFile = document.querySelector('#inputFile');
+
+inputFile.addEventListener('change', event => {
+    // console.log(event.target)
+    // console.log(event.target.value)
+    // console.log(event.target.dataset)
+    const size_in_megabytes = event.target.files[0].size/1024/1024;
     if (size_in_megabytes > 5) {
-        const message = document.querySelector('#inputFile').dataset.message,
-            default_label = document.querySelector('#inputFile').dataset.label;
-        alert(message);
-        document.querySelector('.custom-file-input').value = '';
-        document.querySelector('.custom-file-label').innerHTML = default_label;
+        const dataset = event.target.dataset;
+        alert(dataset.message);
+        event.target.value = '';
+        document.querySelector('.custom-file-label').innerHTML = dataset.defaultLabel;
     } else {
-        const label = $(this).val().replace(/\\/g, '/').replace(/.*\//, '');
+        const label = event.target.value.replace(/\\/g, '/').replace(/.*\//, '');
         document.querySelector('.custom-file-label').innerHTML = label;
     }
 });
