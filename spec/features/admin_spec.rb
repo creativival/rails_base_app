b4abb4641dfed62b80a446b62cb4fc47dad09503
creativial <creativival@gmail.com>
2020-10-26 15:57:22 +0900

    Administrate 管理画面でUserを一覧表示、表示、編集できるテストを書く

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -47,5 +47,48 @@ RSpec.feature "Admins", type: :feature, js: true do
       sleep 1
       expect(page.current_path).to eq admin_root_path
     end
+
+    it '管理画面でユーザーを全員表示できる' do
+      visit admin_root_path
+      expect(page.current_path).to eq admin_root_path
+      page.assert_selector('tr.js-table-row', count: 11)
+      expect(page.all('a.action-show', text: 'admin').size).to eq(1)
+      expect(page.all('a.action-show', text: 'user').size).to eq(32)
+    end
+
+    it '管理画面でユーザーを編集できる' do
+      visit admin_root_path
+      expect(page.all('a.action-show', text: 'admin').size).to eq(1)
+      first('tr.js-table-row').click_link '編集'
+      expect(page.current_path).to eq edit_admin_user_path user11
+      fill_in '名前', with: 'RENAME'
+      fill_in 'メールアドレス', with: 'rename@example.com'
+      execute_script('window.scrollBy(0,10000)') # scroll
+      select 'admin', from: 'Role'
+      click_button '更新する'
+      expect(page.current_path).to eq admin_user_path user11
+      expect(page).to have_content 'RENAME'
+      expect(page).to have_content 'rename@example.com' # unconfirmed_email(登録はされていない)
+      expect(page).to have_content 'admin' # unconfirmed_email
+    end
+
+    it '管理画面でプロフィール画像を変更できる' do
+      file_path = Rails.root.join('spec', 'support', 'assets', 'avatar.png')
+
+      visit edit_admin_user_path user11
+      expect(page.current_path).to eq edit_admin_user_path user11
+      attach_file('user_avatar', file_path)
+      execute_script('window.scrollBy(0,10000)') # scroll
+      click_button '更新する'
+      expect(page.current_path).to eq admin_user_path user11
+      expect(page).to have_css('.flash-notice', text: 'Userを更新しました。')
+      expect(page.find('img')['src']).to have_content 'avatar.png'
+    end
+
+    it '管理画面からアプリに戻れる' do
+      visit admin_root_path
+      click_link 'アプリに戻る'
+      expect(page.current_path).to eq root_path
+    end
   end
 end
